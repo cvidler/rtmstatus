4f2137fc98816c2d92e1da25d4f06ac919890d82
Chris Vidler <christopher.vidler@dynatrace.com>
2016-03-31 15:41:50 +1100

    fix script when run under cron

--- AMDLive/rpt_app.sh
+++ AMDLive/rpt_app.sh
@@ -7,25 +7,29 @@
 # Script needs to run with root permissions (or sufficient rights to run rcmd against the RTM process).
 #
 
+RCMD=/usr/adlex/rtm/bin/rcmd
+AWK=/usr/bin/awk
+DATE=/usr/bin/date
+
 echo -e '<?xml version="1.0" encoding="UTF-8"?>'
 echo -e '<?xml-stylesheet type="text/xsl" href="xslt.xsl"?>'
 echo -e '<amdlive>'
 
 echo -e '<sslist>'
-rcmd lapp sortby srvbytes descending | awk -F'"' '/^\s*$/ {next;} { gsub(/ /,"_",$2) ; print "n:" $2 " a:" $4 "" $5 } ' | awk -F' |:' '{ gsub(/_/," ",$2) ; print "<ss><name>"$2"</name><sessions>"$6"</sessions><srvbytes>"$8"</srvbytes><clibytes>"$10"</clibytes></ss>"  }'
+$RCMD lapp sortby srvbytes descending | $AWK -F'"' '/^\s*$/ {next;} { gsub(/ /,"_",$2) ; print "n:" $2 " a:" $4 "" $5 } ' | $AWK -F' |:' '{ gsub(/_/," ",$2) ; print "<ss><name>"$2"</name><sessions>"$6"</sessions><srvbytes>"$8"</srvbytes><clibytes>"$10"</clibytes></ss>"  }'
 echo -e '</sslist>'
 
 
 echo -e '<srvlist>'
-rcmd lsrv sortby srvbytes descending | awk -F' ' '/^\s*$/ {next;} { gsub(/:/,"%3A",$1) } { print "srv:" $1, $2, $3, $4, $5, $6, $7, $8, $9  } ' | awk -F':| ' '{ gsub(/%3A/,":",$2); print "<srv><name>"$2"</name><sessions>"$4"</sessions><srvbytes>"$6"</srvbytes><clibytes>"$8"</clibytes></srv>"  }'
+$RCMD lsrv sortby srvbytes descending | $AWK -F' ' '/^\s*$/ {next;} { gsub(/:/,"%3A",$1) } { print "srv:" $1, $2, $3, $4, $5, $6, $7, $8, $9  } ' | $AWK -F':| ' '{ gsub(/%3A/,":",$2); print "<srv><name>"$2"</name><sessions>"$4"</sessions><srvbytes>"$6"</srvbytes><clibytes>"$8"</clibytes></srv>"  }'
 echo -e '</srvlist>'
 
 echo -e '<clilist>'
-rcmd lcli sortby clibytes descending | awk -F' ' '/^\s*$/ {next;} { gsub(/:/,"%3A",$1) } { print "cli:" $1, $2, $3, $4, $5, $6, $7, $8, $9  } ' | awk -F':| ' '{ gsub(/%3A/,":",$2); print "<cli><name>"$2"</name><sessions>"$4"</sessions><srvbytes>"$6"</srvbytes><clibytes>"$8"</clibytes></cli>"  }'
+$RCMD lcli sortby clibytes descending | $AWK -F' ' '/^\s*$/ {next;} { gsub(/:/,"%3A",$1) } { print "cli:" $1, $2, $3, $4, $5, $6, $7, $8, $9  } ' | $AWK -F':| ' '{ gsub(/%3A/,":",$2); print "<cli><name>"$2"</name><sessions>"$4"</sessions><srvbytes>"$6"</srvbytes><clibytes>"$8"</clibytes></cli>"  }'
 echo -e '</clilist>'
 
 echo -n '<info><timestamp>'
-echo -n `date`
+echo -n `$DATE`
 echo -e '</timestamp></info>'
 echo -e '</amdlive>'
 
