cfe8aca549187f0906a0968e218936252f6ca62c
cvidler <christopher.vidler@dynatrace.com>
2016-04-04 20:40:39 +1000

    made filter syntax more user friendly

--- AMDLive/table.js
+++ AMDLive/table.js
@@ -598,7 +598,7 @@ var Table = (function(){
 			}
 			// Also allow for a regular input
 			if (filters.nodeName=="INPUT" && filters.type=="text") {
-				filters={ 'filter':"/^"+filters.value+"/" };
+				filters={ 'filter':"/"+filters.value+"/" };
 			}
 			// Force filters to be an array
 			if (typeof(filters)=="object" && !filters.length) {
@@ -611,7 +611,7 @@ var Table = (function(){
 				if (typeof(filter.filter)=="string") {
 					// If a filter string is like "/expr/" then turn it into a Regex
 					if (filter.filter.match(/^\/(.*)\/$/)) {
-						filter.filter = new RegExp(RegExp.$1);
+						filter.filter = new RegExp(RegExp.$1,"i");
 						filter.filter.regex=true;
 					}
 					// If filter string is like "function (x) { ... }" then turn it into a function
